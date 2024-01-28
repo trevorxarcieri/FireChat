9962b474d3d2758b162bc7407dec2561233ee0ae
trevorxarcieri <agent.steel.ta@gmail.com>
2024-01-27 20:05:58 -0500

    Removed packages and updated files based on rollup

--- src/main.js
+++ src/main.js
@@ -1,4 +1,37 @@
-import lodash from 'lodash';
+const searchInputWidgetDivClassName = "RNNXgb";
 
-document.body.style.border = "5px solid blue";
-console.log(lodash.chunk(['a', 'b', 'c', 'd'], 2));
+let handleRefineSearchClick = (refineSearchButton) => {
+    refineSearchButton.addEventListener("click", () => {
+        console.log("Refine search button clicked");
+    });
+}
+
+
+let injectRefineSearchButton = () => {
+    let searchInputWidgetDiv = document.body.getElementsByClassName(searchInputWidgetDivClassName)[0];
+
+    let refineSearchButton = document.createElement("div");
+    refineSearchButton.style.display = "flex";
+    refineSearchButton.style.width = "44px";
+    refineSearchButton.style.height = "44px";
+    refineSearchButton.className = "searchInputButton";
+    refineSearchButton.style.alignItems = "center";
+    refineSearchButton.style.cursor = "pointer";
+
+    let refineSearchButtonSvg = document.createElement("img");
+    refineSearchButtonSvg.src = chrome.runtime.getURL("icons/fire-svgrepo-com.svg");
+    refineSearchButtonSvg.style.width = "24px";
+    refineSearchButtonSvg.style.height = "24px";
+
+    refineSearchButton.appendChild(refineSearchButtonSvg);
+    searchInputWidgetDiv.appendChild(refineSearchButton);
+
+    handleRefineSearchClick(refineSearchButton);
+}
+
+
+var main = () => {
+    injectRefineSearchButton();
+}
+
+main();
