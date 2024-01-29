59f1956e0e92bb70d6edd8b75c1abc92c99cb4db
trevorxarcieri <agent.steel.ta@gmail.com>
2024-01-29 17:41:04 -0500

    began implementation of storing OpenAI API Key

--- settings/options.js
+++ settings/options.js
@@ -1,7 +1,7 @@
 function saveOptions(e) {
     e.preventDefault();
-    browser.storage.sync.set({
-      OPEN_API_KEY: document.querySelector("#OPENAI_API_KEY").value,
+    browser.storage.local.set({
+      "OPEN_API_KEY": document.querySelector("#OPENAI_API_KEY").value,
     });
 }
 
